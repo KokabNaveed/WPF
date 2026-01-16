e2d9bcbb035004e1a76be7b9b497486437401fc5
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2026-01-16 23:10:42 +0500

    Connect DB to Software

--- Services/Software.xaml.cs
+++ Services/Software.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using SubsrciptionSystem.Models;
 
 namespace SubsrciptionSystem
 {
@@ -26,31 +17,43 @@ namespace SubsrciptionSystem
 
         private void cmbPlan_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (cmbPlan.SelectedItem is ComboBoxItem selected)
+            if (cmbPlan.SelectedItem is ComboBoxItem selected &&
+                dpSubscribeddate.SelectedDate != null)
             {
+                DateTime startDate = dpSubscribeddate.SelectedDate.Value;
+
                 switch (selected.Content.ToString())
                 {
                     case "Free":
                         txtamount.Text = "0";
+                        txtamount.IsEnabled = false;
+                        dpRenewalDate.SelectedDate = null;
                         dpRenewalDate.IsEnabled = false;
                         break;
 
                     case "Monthly":
                         txtamount.Text = "10";
-                        dpRenewalDate.IsEnabled = true;
+                        txtamount.IsEnabled = false;
+                        dpRenewalDate.IsEnabled = false;
+                        dpRenewalDate.SelectedDate = startDate.AddMonths(1);
                         break;
 
                     case "Yearly":
                         txtamount.Text = "100";
-                        dpRenewalDate.IsEnabled = true;
+                        txtamount.IsEnabled = false;
+                        dpRenewalDate.IsEnabled = false;
+                        dpRenewalDate.SelectedDate = startDate.AddYears(1);
                         break;
                 }
             }
         }
 
+
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
-            if (dpSubscribeddate.SelectedDate == null ||dpRenewalDate.SelectedDate == null  || txtemail.Text == null || txtSoftwareName.Text == null || cmbCategory.SelectedItem == null || cmbPlan.SelectedItem == null)
+            if ( dpSubscribeddate.SelectedDate == null ||
+                string.IsNullOrWhiteSpace(txtemail.Text ) || 
+                string.IsNullOrWhiteSpace(txtSoftwareName.Text) )
             {
                 MessageBox.Show("Please Enter all details.",
                                 "Validation Error",
@@ -58,6 +61,7 @@ namespace SubsrciptionSystem
                                 MessageBoxImage.Warning);
                 return;
             }
+
             if (cmbPlan.SelectedIndex == 0)
             {
                 MessageBox.Show("Please select a Plan Type.",
@@ -76,17 +80,68 @@ namespace SubsrciptionSystem
                 return;
             }
 
-            DateTime registeredDate = dpSubscribeddate.SelectedDate.Value;
-            DateTime renewalDate = dpRenewalDate.SelectedDate.Value;
+            string plan = cmbPlan.Text;
 
-            if (renewalDate <= registeredDate)
+            if (plan != "Free" && dpRenewalDate.SelectedDate == null)
             {
-                MessageBox.Show("Renewal date must be later than registered date.",
-                                "Invalid Date",
+                MessageBox.Show("Renewal date is required for paid plans.",
+                                "Validation Error",
                                 MessageBoxButton.OK,
-                                MessageBoxImage.Error);
+                                MessageBoxImage.Warning);
                 return;
             }
+
+
+            if (!decimal.TryParse(txtamount.Text, out decimal amount) || amount <= 0)
+            {
+                MessageBox.Show(
+                    "Please enter a valid amount.",
+                    "Validation Error",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+                return;
+            }
+
+            DateTime registeredDate = dpSubscribeddate.SelectedDate.Value;
+
+            if (plan == "Monthly")
+            {
+                dpRenewalDate.SelectedDate = registeredDate.AddMonths(1);
+            }
+
+            else if (plan == "Yearly")
+            {
+                dpRenewalDate.SelectedDate = registeredDate.AddYears(1);
+
+            }
+            else
+            {
+                dpRenewalDate.SelectedDate = null;
+            }
+            DateTime renewalDate = dpRenewalDate.SelectedDate.Value;
+
+
+            using (var db = new AppDbContext())
+            {
+                var software = new SoftwareEntity
+                {
+                    SoftwareName = txtSoftwareName.Text.Trim(),
+                    Email = txtemail.Text.Trim(),
+                    SubscribedDate = dpSubscribeddate.SelectedDate.Value,
+                    RenewalDate = dpRenewalDate.SelectedDate.Value,
+                    Category = cmbCategory.Text.Trim(),
+                    PlanType = cmbPlan.Text.Trim(),
+                    Amount = amount,
+                };
+
+                db.Softwares.Add(software);
+                db.SaveChanges();
+            }
+
+
+            MessageBox.Show("Software Subscription added successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
 
             MainWindow mainWindow = new MainWindow();
             mainWindow.Show();
